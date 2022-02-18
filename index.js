ca8ca48ef6bf4f54054dab98182605d8b2bbd9b2
Mari Zaq <m.zaqareishvili99@gmail.com>
2022-02-18 15:59:07 +0400

    created interaction

---
+++ index.js
@@ -0,0 +1,43 @@
+const greetingDiv = document.querySelector("#greeting");
+const answerDiv = document.querySelector("#answer");
+const humanName = document.querySelector(".inputName");
+const button = document.querySelector("#generate");
+
+function typewriter(div, sentence, i, element) {
+  setTimeout(() => {
+    div.innerHTML += sentence[i];
+    i += 1;
+    if (i < sentence.length) {
+      typewriter(div, sentence, i, element);
+    }
+    if (i === sentence.length) {
+      element.style.display = "block";
+      element.focus();
+    }
+  }, 100);
+}
+
+function greet() {
+  const greet = "Welcome human, what is your name?";
+  typewriter(greetingDiv, greet, 0, humanName);
+}
+
+function input() {
+  humanName.addEventListener("keydown", (event) => {
+    const answer = `Hello ${humanName.value}, this is gonna be your anime character!`;
+    if (event.which === 13) {
+      typewriter(answerDiv, answer, 0, button);
+      humanName.disabled = true;
+    }
+  });
+}
+
+function chooseYesOrNO() {}
+
+function start() {
+  greet();
+  input();
+  chooseYesOrNO();
+}
+
+start();
