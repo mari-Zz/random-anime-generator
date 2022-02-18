224ff83573b1d1a6ee978edc8a8bc615f808c084
Mari Zaq <m.zaqareishvili99@gmail.com>
2022-02-18 18:55:15 +0400

    added random anime generator

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const answerDiv = document.querySelector("#answer");
 const humanName = document.querySelector(".inputName");
 const button = document.querySelector("#generate");
 
+const imageElement = document.querySelector(".img");
+const titleElement = document.querySelector(".title");
+const card = document.querySelector(".card");
+
 function typewriter(div, sentence, i, element) {
   setTimeout(() => {
     div.innerHTML += sentence[i];
@@ -22,22 +26,28 @@ function greet() {
   typewriter(greetingDiv, greet, 0, humanName);
 }
 
-function input() {
-  humanName.addEventListener("keydown", (event) => {
-    const answer = `Hello ${humanName.value}, generate your anime character!`;
-    if (event.which === 13) {
-      typewriter(answerDiv, answer, 0, button);
-      humanName.disabled = true;
-    }
-  });
-}
+greet();
 
-function chooseYesOrNO() {}
+humanName.addEventListener("keydown", (event) => {
+  const answer = `Hello ${humanName.value}, generate random anime to watch!`;
+  if (event.which === 13) {
+    typewriter(answerDiv, answer, 0, button);
+    humanName.disabled = true;
+  }
+});
 
-function start() {
-  greet();
-  input();
-  chooseYesOrNO();
-}
+button.addEventListener("click", async () => {
+  let i = Math.floor(Math.random() * (154 - 0 + 1) + 0);
+  const data = await fetch(`https://api.jikan.moe/v4/anime/1/recommendations`);
+  const animes = await data.json();
+  const anime = animes.data[i].entry;
+  console.log(anime);
 
-start();
+  const img = document.createElement("img");
+  img.setAttribute("src", anime.images.jpg.image_url);
+  imageElement.appendChild(img);
+
+  titleElement.innerHTML = anime.title;
+  card.style.display = "flex";
+  button.disabled = true;
+});
