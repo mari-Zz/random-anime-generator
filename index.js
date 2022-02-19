4e127b547c679178577eab70cc06468ffa0e0d26
Mari Zaq <m.zaqareishvili99@gmail.com>
2022-02-19 16:50:44 +0400

    added new api and genre filter

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const button = document.querySelector("#generate");
 const imageElement = document.querySelector("#image");
 const titleElement = document.querySelector(".title");
 const card = document.querySelector(".card");
+const genresDiv = document.querySelector(".genres");
+const selectElement = document.querySelector("#genres");
 
 function typewriter(div, sentence, i, element) {
   setTimeout(() => {
@@ -29,30 +31,54 @@ function greet() {
 greet();
 
 humanName.addEventListener("keydown", (event) => {
-  const answer = `Hello ${humanName.value}, here's Anime recomendation for you!`;
+  const answer = `Hello ${humanName.value}, choose you genre!`;
   if (event.which === 13) {
-    typewriter(answerDiv, answer, 0, button);
+    typewriter(answerDiv, answer, 0, genresDiv);
     humanName.disabled = true;
   }
 });
 
+async function genreButtons() {
+  const data = await fetch(`https://api.jikan.moe/v4/genres/anime`);
+  const result = await data.json();
+  const genres = result.data;
+  genres.forEach((genre) => {
+    let option = document.createElement("option");
+    option.setAttribute("value", genre.mal_id);
+    option.innerHTML = genre.name;
+    selectElement.appendChild(option);
+  });
+}
+
+genreButtons();
+
 function animation() {
   let tl = gsap.timeline();
-
   tl.fromTo(".card", { opacity: 0, y: 20, duration: 2 }, { opacity: 1, y: 0 });
   tl.fromTo(".img", { opacity: 0, y: 20, duration: 1 }, { opacity: 1, y: 0 });
   tl.fromTo(".title", { opacity: 0, y: 20, duration: 1 }, { opacity: 1, y: 0 });
 }
 
 button.addEventListener("click", async () => {
-  let i = Math.floor(Math.random() * (100 - 0 + 1) + 0);
-  let j = Math.random() >= 0.5 ? 1 : 0;
-  const data = await fetch(`https://api.jikan.moe/v4/recommendations/anime`);
+  let i = Math.floor(Math.random() * (25 - 0 + 1) + 0);
+  let selectedGenre = selectElement.value;
+  const data = await fetch(
+    `https://api.jikan.moe/v4/anime?genres=${selectedGenre}`
+  );
   const animes = await data.json();
-  const anime = animes.data[i].entry[j];
+  const anime = animes.data[i];
 
   imageElement.setAttribute("src", anime.images.jpg.image_url);
   titleElement.innerHTML = anime.title;
   animation();
   card.style.display = "flex";
 });
+
+async function fetchTedt() {
+  const data = await fetch("https://animechan.vercel.app/api/available/anime");
+  const result = await data.json();
+
+  console.log(result);
+}
+
+fetchTedt();
